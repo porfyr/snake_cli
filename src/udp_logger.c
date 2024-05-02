9ddcb3f30355d054d7589a82928d1885f6efd963
porfyr <rock097abc@gmail.com>
2024-05-02 20:02:03 +0300

    added udp_logge.c and changed game_arch.h

---
+++ src/udp_logger.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include "game_arch.h"
+
+#define PORT 12345
+#define BUFFER_SIZE 1024
+
+int udp_log(int msg_len, char* msg) {
+
+    char* hello = "hello from client";
+    struct sockaddr_in servaddr = {0};
+    
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd == -1) {
+        perror("failed to create client socket");
+        return -1;
+        // exit(EXIT_FAILURE);
+    }
+
+    servaddr.sin_family = AF_INET; // IPv4
+    servaddr.sin_port = htons(12345);
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");  // INADDR_ANY; // 0.0.0.0 ?
+
+    int len = sendto(sockfd, (const char*)hello, strlen(hello), 0, (const struct sockaddr*)&servaddr, sizeof(servaddr));
+
+    return 0;
+}
